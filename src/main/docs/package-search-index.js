09882637c3d1a7591b7433bf38f9fa5bd701c6fb
Iaroslav S <ziro-x-100@mail.ru>
2023-11-18 15:03:58 +0300

    Initial commit

---
+++ src/main/docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.example.lesson_1.other_class"},{"l":"org.example.lesson_1.sample"}]
---
+++ src/main/docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.example.lesson_1.sample","l":"App"},{"p":"org.example.lesson_1.other_class","l":"Decorator"},{"p":"org.example.lesson_1.other_class","l":"OtherClass"}]
